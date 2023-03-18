5bd669552ca756ad9d314142f8feaf7f2d70425c
styfenschaer <79762922+styfenschaer@users.noreply.github.com>
2023-03-18 23:37:39 +0100

    add F2C and C2F transforms

--- base.cpp
+++ base.cpp
@@ -34,10 +34,16 @@ public:
      void init_plan(size_t n_features);
 
      template <class T>
-     py::array_t<T, C_CONTIGUOUS> CC(const py::array_t<T, C_CONTIGUOUS> &X) const;
+     py::array_t<T, C_CONTIGUOUS> C2C(const py::array_t<T, C_CONTIGUOUS> &X) const;
 
      template <class T>
-     py::array_t<T, F_CONTIGUOUS> FF(const py::array_t<T, F_CONTIGUOUS> &X) const;
+     py::array_t<T, F_CONTIGUOUS> C2F(const py::array_t<T, C_CONTIGUOUS> &X) const;
+
+     template <class T>
+     py::array_t<T, F_CONTIGUOUS> F2F(const py::array_t<T, F_CONTIGUOUS> &X) const;
+
+     template <class T>
+     py::array_t<T, C_CONTIGUOUS> F2C(const py::array_t<T, F_CONTIGUOUS> &X) const;
 };
 
 PolynomialFeatures::PolynomialFeatures(size_t degree,
@@ -59,7 +65,7 @@ void PolynomialFeatures::init_plan(size_t n_features)
 }
 
 template <class T>
-py::array_t<T, C_CONTIGUOUS> PolynomialFeatures::CC(const py::array_t<T, C_CONTIGUOUS> &X) const
+py::array_t<T, C_CONTIGUOUS> PolynomialFeatures::C2C(const py::array_t<T, C_CONTIGUOUS> &X) const
 {
      size_t n_samples = X.shape(0);
      size_t n_features_in = X.shape(1);
@@ -100,11 +106,11 @@ py::array_t<T, C_CONTIGUOUS> PolynomialFeatures::CC(const py::array_t<T, C_CONTI
 
                for (auto j = 0; j < plan.shape(0); ++j)
                {
-                    auto idx1 = *plan_data_i++;
-                    auto idx2 = *plan_data_i++;
-                    auto idx3 = *plan_data_i++;
+                    auto col1 = *plan_data_i++;
+                    auto col2 = *plan_data_i++;
+                    auto col3 = *plan_data_i++;
 
-                    XT_data_i[idx1] = XT_data_i[idx2] * XT_data_i[idx3];
+                    XT_data_i[col1] = XT_data_i[col2] * XT_data_i[col3];
                }
           }
      }
@@ -115,11 +121,67 @@ py::array_t<T, C_CONTIGUOUS> PolynomialFeatures::CC(const py::array_t<T, C_CONTI
 }
 
 template <class T>
-py::array_t<T, F_CONTIGUOUS> PolynomialFeatures::FF(const py::array_t<T, F_CONTIGUOUS> &X) const
+py::array_t<T, C_CONTIGUOUS> PolynomialFeatures::F2C(const py::array_t<T, F_CONTIGUOUS> &X) const
 {
-     const size_t n_samples = X.shape(0);
-     const size_t n_features_in = X.shape(1);
-     const size_t n_features_out = plan.shape(0) + n_features_in + include_bias;
+     size_t n_samples = X.shape(0);
+     size_t n_features_in = X.shape(1);
+     size_t n_features_out = plan.shape(0) + n_features_in + include_bias;
+
+     auto XT = py::array_t<T, C_CONTIGUOUS>({n_samples, n_features_out});
+
+     PY_GIL_RELEASE
+
+     auto X_stride = X.strides(1) / sizeof(T);
+     auto XT_stride = XT.strides(0) / sizeof(T);
+
+     auto X_data = X.data();
+     auto XT_data = XT.mutable_data();
+     auto XT_data_b = XT_data + include_bias;
+     auto plan_data = plan.data();
+
+#pragma omp parallel num_threads(n_jobs)
+     {
+#pragma omp for
+          for (auto i = 0; i < n_samples; ++i)
+          {
+               if (include_bias)
+               {
+                    XT_data[i * XT_stride] = 1;
+               }
+
+               auto XT_data_i = XT_data_b + i * XT_stride;
+               auto X_data_i = X_data + i;
+
+               for (auto j = 0; j < n_features_in; ++j)
+               {
+                    XT_data_i[j] = X_data_i[j * X_stride];
+               }
+
+               XT_data_i = XT_data + i * XT_stride;
+               auto plan_data_i = plan_data;
+
+               for (auto j = 0; j < plan.shape(0); ++j)
+               {
+                    auto col1 = *plan_data_i++;
+                    auto col2 = *plan_data_i++;
+                    auto col3 = *plan_data_i++;
+
+                    XT_data_i[col1] = XT_data_i[col2] * XT_data_i[col3];
+               }
+          }
+     }
+
+     PY_GIL_ACQUIRE
+
+     return XT;
+}
+
+template <class T>
+py::array_t<T, F_CONTIGUOUS> PolynomialFeatures::F2F(const py::array_t<T, F_CONTIGUOUS> &X) const
+{
+     size_t n_samples = X.shape(0);
+     size_t n_features_in = X.shape(1);
+     size_t n_features_out = plan.shape(0) + n_features_in + include_bias;
 
      auto XT = py::array_t<T, F_CONTIGUOUS>({n_samples, n_features_out});
 
@@ -179,6 +241,71 @@ py::array_t<T, F_CONTIGUOUS> PolynomialFeatures::FF(const py::array_t<T, F_CONTI
      return XT;
 }
 
+template <class T>
+py::array_t<T, F_CONTIGUOUS> PolynomialFeatures::C2F(const py::array_t<T, C_CONTIGUOUS> &X) const
+{
+     size_t n_samples = X.shape(0);
+     size_t n_features_in = X.shape(1);
+     size_t n_features_out = plan.shape(0) + n_features_in + include_bias;
+
+     auto XT = py::array_t<T, F_CONTIGUOUS>({n_samples, n_features_out});
+
+     PY_GIL_RELEASE
+
+     auto X_stride = X.strides(0) / sizeof(T);
+     auto XT_stride = XT.strides(1) / sizeof(T);
+     auto plan_stride = plan.strides(0) / sizeof(int64_t);
+
+     auto X_data = X.data();
+     auto XT_data = XT.mutable_data();
+     auto XT_data_b = XT_data + include_bias * XT_stride;
+     auto plan_data = plan.data();
+
+#pragma omp parallel num_threads(n_jobs)
+     {
+          if (include_bias)
+          {
+#pragma omp for nowait
+               for (auto i = 0; i < n_samples; ++i)
+               {
+                    XT_data[i] = 1;
+               }
+          }
+
+#pragma omp for
+          for (auto i = 0; i < n_samples; ++i)
+          {
+               auto X_data_i = X_data + i * X_stride;
+               auto XT_data_i = XT_data_b + i;
+               for (auto j = 0; j < n_features_in; ++j)
+               {
+                    XT_data_i[j * XT_stride] = X_data_i[j];
+               }
+          }
+
+          for (auto j = 0; j < plan.shape(0); ++j)
+          {
+               auto col1 = plan_data[j * plan_stride + 0];
+               auto col2 = plan_data[j * plan_stride + 1];
+               auto col3 = plan_data[j * plan_stride + 2];
+
+               auto XT_data_j1 = XT_data + col1 * XT_stride;
+               auto XT_data_j2 = XT_data + col2 * XT_stride;
+               auto XT_data_j3 = XT_data + col3 * XT_stride;
+
+#pragma omp for
+               for (auto i = 0; i < n_samples; ++i)
+               {
+                    XT_data_j1[i] = XT_data_j2[i] * XT_data_j3[i];
+               }
+          }
+     }
+
+     PY_GIL_ACQUIRE
+
+     return XT;
+}
+
 PYBIND11_MODULE(base, handle)
 {
      py::class_<PolynomialFeatures>(handle, "PolynomialFeatures")
@@ -189,13 +316,21 @@ PYBIND11_MODULE(base, handle)
               py::arg("n_jobs"))
          .def("init_plan", &PolynomialFeatures::init_plan,
               py::arg("n_features"))
-         .def("CC32", &PolynomialFeatures::CC<float32_t>,
+         .def("C2C32", &PolynomialFeatures::C2C<float32_t>,
               py::arg("X"))
-         .def("CC64", &PolynomialFeatures::CC<float64_t>,
+         .def("C2C64", &PolynomialFeatures::C2C<float64_t>,
               py::arg("X"))
-         .def("FF32", &PolynomialFeatures::FF<float32_t>,
+         .def("F2C32", &PolynomialFeatures::F2C<float32_t>,
               py::arg("X"))
-         .def("FF64", &PolynomialFeatures::FF<float64_t>,
+         .def("F2C64", &PolynomialFeatures::F2C<float64_t>,
+              py::arg("X"))
+         .def("F2F32", &PolynomialFeatures::F2F<float32_t>,
+              py::arg("X"))
+         .def("F2F64", &PolynomialFeatures::F2F<float64_t>,
+              py::arg("X"))
+         .def("C2F32", &PolynomialFeatures::C2F<float32_t>,
+              py::arg("X"))
+         .def("C2F64", &PolynomialFeatures::C2F<float64_t>,
               py::arg("X"))
          .def_readwrite("interaction_only", &PolynomialFeatures::interaction_only)
          .def_readwrite("include_bias", &PolynomialFeatures::include_bias)
